084c0da19f9e95b76e841949c4d5de6e479d47be
mufti <iwak.sotong@gmail.com>
2017-03-08 00:19:07 +0700

    the button can start container now..

--- spec/docker_manager_spec.js
+++ spec/docker_manager_spec.js
@@ -1,17 +1,25 @@
 describe("DockerManager", function() {
   var dockerManager,
-      spyLoadContainers;
-  var fakeReply = [{
-    Names: ['/Nama kontainer'],
-    Image: 'gambar/cakep',
-    Status: 'Hidup'
-  }];
-
+      spyLoadContainers,
+      fakeReply,
+      startContainerButton,
+      stopContainerButton;
 
   beforeEach(function() {
+    htmlContainer = affix('.docker-manager__container-list');
+    startContainerButton = affix('button.docker-manager__container-action--start[data-container-id=myFakeContainerId12345]');
+    stopContainerButton = affix('button.docker-manager__container-action--stop');
+
     //we dont want it to calls ajax evrytime DockerManager instatiated
     spyLoadContainers = spyOn(DockerManager.prototype, 'loadContainers');
-    htmlContainer = affix('.docker-manager__container-list');
+
+    fakeReply  = [{
+      Id: 'abcd12345',
+      Names: ['/Nama kontainer'],
+      Image: 'gambar/cakep',
+      Status: 'Hidup',
+      State: 'running',
+    }];
   });
 
   describe('#constructor', function() {
@@ -23,6 +31,20 @@ describe("DockerManager", function() {
     it('calls loadContainers', function() {
       dockerManager = new DockerManager();
       expect(dockerManager.loadContainers).toHaveBeenCalled();
+    });
+
+    it('bind click event on start container buttons', function() {
+      spyOn(DockerManager.prototype, 'startContainer');
+      dockerManager = new DockerManager();
+      startContainerButton.click();
+      expect(dockerManager.startContainer).toHaveBeenCalled();
+    });
+
+    it('bind click event on stop container buttons', function() {
+      spyOn(DockerManager.prototype, 'stopContainer');
+      dockerManager = new DockerManager();
+      stopContainerButton.click();
+      expect(dockerManager.stopContainer).toHaveBeenCalled();
     });
   });
 
@@ -70,6 +92,68 @@ describe("DockerManager", function() {
 
       expect(containerItem).toContainElement('.docker-manager__container-status');
       expect(containerItem.children('.docker-manager__container-status')).toHaveText('Hidup');
+    });
+
+    it('add start/stop button for containers', function() {
+      dockerManager = new DockerManager();
+      spyOn(dockerManager, 'createStartStopButton');
+      dockerManager.displayContainers(fakeReply);
+
+      expect(dockerManager.createStartStopButton).toHaveBeenCalledWith(fakeReply[0]);
+    });
+  });
+
+  describe('#createStartStopButton', function() {
+    it('create button having attribute data-container-id', function() {
+      dockerManager = new DockerManager();
+
+      var startStopButton = dockerManager.createStartStopButton(fakeReply[0]);
+      expect(startStopButton.data('container-id')).toEqual('abcd12345');
+    });
+
+    it('display button to stop container if status is `running`', function() {
+      dockerManager = new DockerManager();
+      fakeReply[0].State = 'running';
+
+      var startStopButton = dockerManager.createStartStopButton(fakeReply[0]);
+
+      expect(startStopButton).toHaveClass('docker-manager__container-action');
+      expect(startStopButton).toHaveClass('docker-manager__container-action--stop');
+      expect(startStopButton.data('action')).toEqual('stop');
+      expect(startStopButton).toHaveText('Stop');
+    });
+
+    it('display button to start container if status is `exited`', function() {
+      dockerManager = new DockerManager();
+      fakeReply[0].State = 'exited';
+
+      var startStopButton = dockerManager.createStartStopButton(fakeReply[0]);
+
+      expect(startStopButton).toHaveClass('docker-manager__container-action');
+      expect(startStopButton).toHaveClass('docker-manager__container-action--start');
+      expect(startStopButton.data('action')).toEqual('start');
+      expect(startStopButton).toHaveText('Start');
+    });
+  });
+
+  describe('#startContainer', function() {
+
+    beforeEach(function() {
+      var fakeEvent = { target: startContainerButton };
+
+      jasmine.Ajax.install();
+      dockerManager = new DockerManager();
+      dockerManager.startContainer(fakeEvent);
+    });
+
+    afterEach(function() {
+      jasmine.Ajax.uninstall();
+    });
+
+    it('call ajax to start container with id from data-container-id', function() {
+      var request = jasmine.Ajax.requests.mostRecent();
+      expect(request.method).toEqual('POST');
+      expect(request.url).toEqual('/containers/myFakeContainerId12345/start');
     });
   });
 });
--- src/docker_manager.js
+++ src/docker_manager.js
@@ -2,9 +2,14 @@ export default class DockerManager {
   constructor() {
     this.sel = {
       containerList: '.docker-manager__container-list',
-      containerItem: '.docker-manager__container-item'
+      containerItem: '.docker-manager__container-item',
+      startContainerButtons: '.docker-manager__container-action--start',
+      stopContainerButtons: '.docker-manager__container-action--stop'
     };
     this.loadContainers();
+
+    $('body').bind('click', this.startContainerButtons, this.startContainer.bind(this));
+    $('body').bind('click', this.stopContainerButtons, this.stopContainer.bind(this));
   }
 
   loadContainers() {
@@ -13,15 +18,44 @@ export default class DockerManager {
 
   displayContainers(containers) {
     containers.forEach(function eachContainer(container) {
-      var containerView = `
+      var containerView = $(`
         <div class=docker-manager__container-item>
           <div class=docker-manager__container-name>${container.Names[0].slice(1)}</div>
           <div class=docker-manager__container-image>${container.Image}</div>
           <div class=docker-manager__container-status>${container.Status}</div>
         </div>
-      `;
+      `);
 
+      containerView.append(this.createStartStopButton(container));
       $(this.sel.containerList).append(containerView);
     }.bind(this));
   }
+
+  createStartStopButton(container) {
+    var startStopButton =  $('<button class=docker-manager__container-action></button>');
+    startStopButton.attr('data-container-id', container.Id);
+
+    if(container.State === 'running') {
+      startStopButton.addClass('docker-manager__container-action--stop');
+      startStopButton.attr('data-action', 'stop');
+      startStopButton.html('Stop');
+    } else {
+      startStopButton.addClass('docker-manager__container-action--start');
+      startStopButton.attr('data-action', 'start');
+      startStopButton.html('Start');
+    }
+
+    return startStopButton;
+  }
+
+  startContainer(e) {
+    console.log(e)
+    var target = $(e.target);
+    var containerId = target.data('containerId');
+    if(containerId) {
+      $.post(`/containers/${containerId}/start`);
+    }
+  }
+
+  stopContainer(){}
 }
